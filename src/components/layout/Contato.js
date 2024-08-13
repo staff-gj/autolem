97a448096b13b010a3a19d73efb3231eebffb09a
Kaio <115432893+staff-gj@users.noreply.github.com>
2024-08-12 22:15:50 -0300

    Contato.js

--- src/components/layout/Contato.js
+++ src/components/layout/Contato.js
@@ -1,9 +1,6 @@
 import './Contato.css';
 import Footer from "./Footer";
 import Layout from "./Layout";
-import { FloatingWhatsApp } from 'react-floating-whatsapp';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import logo from "../../img/logo.jpg";
 
 
@@ -12,18 +9,6 @@ export default function Contato() {
     return (
         <div>
             <Layout />
-            <FloatingWhatsApp 
-                phoneNumber= "77998343655" 
-                accountName="Nova AutoLem" 
-                allowEsc 
-                allowClickAway 
-                notification = {true}
-                notificationSound = {true}
-                avatar={logo}
-                statusMessage='Online'
-                placeholder='Eu quero me matricular agora!!'
-                chatMessage="Olá, tudo bem com você? Venha já para a Auto escola de qualidade que você merece! Entre em contato clicando abaixo"
-                />
 <div className='contatopag'>
             <div className="custom-attendance">
                 <div className="custom-attendance-header">
